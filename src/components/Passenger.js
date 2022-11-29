1e8a121d5972c7f8cc72dc649f081f2decf2fbd3
Georgia Asimaki <Georgia.Asimaki@solera.com>
2022-11-29 16:18:59 +0200

    Passenger v3

--- src/components/Passenger.js
+++ src/components/Passenger.js
@@ -20,127 +20,125 @@ import Switch from './Switch'
 
 export default function Passenger() {
 
-// States for registration
-const [firstname, setFirstName] = useState('');
-const [lastname, setLastName] = useState('');
-const [nationality, setNation] = useState('');
-const [age, setAge] = useState('');
-const [id,setId] = useState('');
-const [value, setValue] = React.useState(false);
+    // States for registration
+    const [firstname, setFirstName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [nationality, setNation] = useState('');
+    const [age, setAge] = useState('');
+    const [id,setId] = useState('');
+    const [value, setValue] = React.useState(false);
 
 
-// Handling the name change
-const handleFirstName = (e) => {
-	setFirstName(e.target.value);
+    // Handling the name change
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
 
-};
+    };
 
-const handleLastName = (e) => {
-	setLastName(e.target.value);
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
 
-};
+    };
 
-const handleNation = (e) => {
-	setNation(e.target.value);
+    const handleNation = (e) => {
+        setNation(e.target.value);
 
-};
+    };
 
-const handleAge = (e) => {
-	setAge(e.target.value);
+    const handleAge = (e) => {
+        setAge(e.target.value);
 
-};
+    };
 
-const handleId = (e) => {
-    setId(e.target.value);
-}
+    const handleId = (e) => {
+        setId(e.target.value);
+    }
 
-return (
-    <Grid container>
-        <Grid item xs={12}>
-            <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
 
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="First Name"
-                    onChange={handleFirstName}
-                    value={firstname}
-                />
-            </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-         <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
-            <TextField
-                required
-                id="outlined-required"
-                label="Last Name"
-                onChange={handleLastName}
-                value={lastname}
-            />
-        </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-            <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
-            <TextField
-                required
-                id="outlined-required"
-                label="Nationality"
-                onChange={handleNation}
-                value={nationality}
-            />
-        </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-            <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Identification"
-                    onChange={handleId}
-                    value={id}
-                />
-            <FormHelperText>Identification (NIF or passport)</FormHelperText>
-            </FormControl>
-        </Grid>
-        <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="First Name"
+                        onChange={handleFirstName}
+                        value={firstname}
+                    />
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
              <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
-                <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-helper-label"
-                        id="demo-simple-select-helper"
-                        value={3}
-                        label="Age"
-                        onChange={handleAge}
-                    >
-                        <MenuItem value={1}> less than 2 years </MenuItem>
-                        <MenuItem value={2}> between 2 and 9 years</MenuItem>
-                        <MenuItem value={3}> more than 9 years</MenuItem>
-
-                    </Select>
-                <FormHelperText>Choose your Age range</FormHelperText>
-             </FormControl>
-         </Grid>
-
-             <Grid item xs={12}>
-                            <FormControl sx={{m: 2, top:20,  minWidth: 220 }}>
-                                <Button variant="contained" href="complete">
-                                    SUBMIT
-                                </Button>
-                            </FormControl>
-                          </Grid>
-
-         <Grid item xs={12}>
-            <FormControl sx={{m: 2, top:20,  minWidth: 220 }}>
-                <Switch isOn={value} handleToggle={() => {setValue(!value);}}/>
-                <FormHelperText>Will you be carrying bags?</FormHelperText>
-                {value && (
-                <h6 style={{ color: 'red' , font: 'Arial'}}>Warning! There might be extra charges.</h6>
-                )}
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Last Name"
+                    onChange={handleLastName}
+                    value={lastname}
+                />
             </FormControl>
-         </Grid>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Nationality"
+                    onChange={handleNation}
+                    value={nationality}
+                />
+            </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Identification"
+                        onChange={handleId}
+                        value={id}
+                    />
+                <FormHelperText>Identification (NIF or passport)</FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+                 <FormControl sx={{m: 2, top:20,  minWidth: 220 }} >
+                    <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-helper-label"
+                            id="demo-simple-select-helper"
+                            value={3}
+                            label="Age"
+                            onChange={handleAge}
+                        >
+                            <MenuItem value={1}> less than 2 years </MenuItem>
+                            <MenuItem value={2}> between 2 and 9 years</MenuItem>
+                            <MenuItem value={3}> more than 9 years</MenuItem>
 
-     </Grid>
+                        </Select>
+                    <FormHelperText>Choose your Age range</FormHelperText>
+                 </FormControl>
+             </Grid>
+             <Grid item xs={12}>
+                <FormControl sx={{m: 2, top:10,  minWidth: 220 }}>
+                    <Switch isOn={value} handleToggle={() => {setValue(!value);}}/>
+                    <FormHelperText>Will you be carrying bags?</FormHelperText>
+                    {value && (
+                        <h6 style={{ color: 'red' , font: 'Arial'}}>Warning! There might be extra charges.</h6>
+                    )}
+                </FormControl>
+             </Grid>
+             <Grid item xs={12}>
+                <FormControl sx={{m: 2,  minWidth: 220 }}>
+                    <Button variant="contained" href="complete" >
+                        SUBMIT
+                    </Button>
+                </FormControl>
+             </Grid>
 
-     );
+        </Grid>
+
+    );
 
 }
