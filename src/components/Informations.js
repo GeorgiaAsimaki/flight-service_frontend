bee32b9e6b3369701b67b3bdd81ef02af81e7d2e
Georgia Asimaki <Georgia.Asimaki@solera.com>
2022-11-29 17:56:10 +0200

    Results v1

---
+++ src/components/Informations.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+const Informations = ({company, companyF, flightNum, timeDep, dateDep, timeTransit, layover, layoverF, layoverNum, luggage, luggageF, price}) => {
+  return (
+    <>
+        {(companyF=='All' || company==companyF)  && ((layoverF===true && layover===layoverNum)|| layoverF===false) && (luggageF===false || (luggageF===true && luggage)) && (
+                <Card sx={{ minWidth: 175 }} variant="outlined">
+                  <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                      {company}
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                      {flightNum}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {dateDep} {timeDep}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Transit time: {timeTransit}
+                      <br />
+                      Layover: {layover}
+                      <br />
+                      Luggage allowance in cabin:
+                      {luggage && (" yes")}
+                      {!luggage && (" no")}
+                      <br />
+                      Total price: {price}
+                    </Typography>
+                  </CardContent>
+                </Card>
+        )}
+
+    </>
+  );
+}
+
+export default Informations;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,57 @@
+import * as React from 'react';
+import { Grid, Typography } from '@mui/material';
+import Informations from './Informations';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 
 const Results = () => {
+    const [comp,setComp]=React.useState('All');
+    const [scales,setScales]=React.useState(false);
+    const [lug,setLuggage]=React.useState(false);
+    const [num,setNum]=React.useState("0");
 
     return(
-        <p></p>
+        <Box sx={{ width: '100%' }}>
+            <Stack spacing={2}>
+                <Informations
+                    company="AEGEAN"
+                    companyF={comp}
+                    flightNum = "AG10233"
+                    timeDep = "16:10"
+                    dateDep = "Monday"
+                    timeTransit = "15:00"
+                    layover = "2"
+                    layoverF = {scales}
+                    layoverNum = {num}
+                    luggage = {true}
+                    luggageF = {lug}
+                    price = "1000" >
+                </Informations>
+                <Informations
+                    company="IBERIA"
+                    companyF={comp}
+                     flightNum = "IB10233"
+                     timeDep = "18:10"
+                     dateDep = "Monday"
+                     timeTransit = "14:00"
+                     layover = "0"
+                     layoverF = {scales}
+                     layoverNum = {num}
+                     luggage = {false}
+                     luggageF = {lug}
+                     price = "900"
+
+                     >
+                </Informations>
+
+            </Stack>
+
+            </Box>
+
     );
 
 }
